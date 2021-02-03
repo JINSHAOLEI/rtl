7c76057a46634f5d12588bdf1d33fe537d6e409a
JINSHAOLEI <43226393+JINSHAOLEI@users.noreply.github.com>
2021-02-03 14:18:04 +0800

    Update generate.sv

--- generate.sv
+++ generate.sv
@@ -17,14 +17,14 @@ endmodule
 generate
 if(WIDTH<10)
     assign a = b ;
-	/*always@(*)begin
-	    a = b;
-	end*/
+    /*always@(*)begin
+    a = b;
+    end*/
 else
     assign a = c ;
-	/*always@(*)begin
-	    a = c;
-	end*/
+    /*always@(*)begin
+    a = c;
+    end*/
 endgenerate
 //////// 3: intance////////
 parameter NUM_OF_PORTS =16;
@@ -32,12 +32,12 @@ generate
      genvar n;
 	 for(n=0;n<NUM_OF_PORTS;n=n+1)
 	 begin:switch_port_inst
-		switch_port swicth_port_u
-		(.a(in1[n])  ,
+	 switch_port swicth_port_u
+	         (.a(in1[n])  ,
 		 .b(in2[n])  ,
 		 .c(out1[n]) ,
 		 .d(out2[n])
-	    ); 
+	          ); 
 	 end
 endgenerate
 	 
